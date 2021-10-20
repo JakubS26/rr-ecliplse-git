8bf92a4bcddd8ef0faeaf5bccd6971f71263d038
JakubS26 <jakub_sokolowski@onet.pl>
2021-10-20 22:12:41 +0200

    Zmiana nr 3

--- Rysowanie3/src/rysowanie/Main.java
+++ Rysowanie3/src/rysowanie/Main.java
@@ -18,6 +18,7 @@ public class Main {
     
     public static void main(String args []){
     	
+    	
         JFrame okno = new JFrame("Program do rysowania");
         Surface Tlo = new Surface();
         okno.setBounds(450, 100, 1000, 800);
